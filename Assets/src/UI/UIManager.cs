b9425f5889ad0fd7f80aea634de70c47def4e711
kyoichi001 <tanso0730@gmail.com>
2023-07-05 15:41:54 +0900

    目次作成

--- Assets/src/UI/UIManager.cs
+++ Assets/src/UI/UIManager.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Xml.Linq;
+using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEngine;
 using UnityEngine.UIElements;
+using static System.Collections.Specialized.BitVector32;
 using static UnityEditor.Rendering.FilterWindow;
 
 public class UIManager : MonoBehaviour
@@ -37,6 +39,41 @@ public class UIManager : MonoBehaviour
     {
         return next.indent == target.indent + 1;
     }
+    void generateTableOfContentDOM(VisualElement root, List<Section> sections)
+    {
+        var indentContainer = new List<VisualElement> { root };
+        for (var i = 0; i < sections.Count - 1; i++)
+        {
+            var sectionData = sections[i];
+            if (hasChild(sectionData, sections[i + 1]))
+            {
+                //セクションが子要素を含むならfoldoutにする
+                var section = foldableSectionUXML.CloneTree();
+                section.Q<Foldout>("header").text = sectionData.header + "  " + sectionData?.header_text;
+                indentContainer[sectionData.indent - 1].Add(section);
+                if (indentContainer.Count <= sectionData.indent)
+                {
+                    indentContainer.Add(section.Q<VisualElement>("unity-content"));
+                }
+                else
+                {
+                    indentContainer[sectionData.indent] = section.Q<VisualElement>("unity-content");
+                }
+            }
+            else if(sectionData.header_text?.Length>0)
+            {
+                var section = sectionUXML.CloneTree();
+                section.Q<Label>("header").text = sectionData.header+sectionData.header_text;
+                section.Q<VisualElement>("sectionContainer").Remove(section.Q<VisualElement>("rightContainer"));
+                indentContainer[sectionData.indent - 1].Add(section);
+            }
+        }
+        var sectionDat = sections[sections.Count-1];
+        var sectionDOM = sectionUXML.CloneTree();
+        sectionDOM.Q<Label>("header").text = sectionDat.header + sectionDat.header_text;
+        sectionDOM.Q<VisualElement>("sectionContainer").Remove(sectionDOM.Q<VisualElement>("rightContainer"));
+        indentContainer[sectionDat.indent - 1].Add(sectionDOM);
+    }
 
     void generateSectionsDOM(VisualElement root, List<Section> sections)
     {
@@ -54,9 +91,12 @@ public class UIManager : MonoBehaviour
                 section.Q<Foldout>("header").text = sectionData.header + "  " + sectionData?.header_text;
                 foreach (var text in sectionData.texts)
                 {
-                    var textDOM = new Label();
-                    textDOM.text = text.raw_text;
-                    section.Q<VisualElement>("childContainer").Add(textDOM);
+                    foreach(var token in text.tokens)
+                    {
+                        var tokenDOM = tokenUXML.CloneTree();
+                        tokenDOM.Q<Label>().text = token.text;
+                        section.Q<VisualElement>("childContainer").Add(tokenDOM);
+                    }
                 }
                 indentContainer[sectionData.indent - 1].Add(section);
                 if (indentContainer.Count <= sectionData.indent)
@@ -118,5 +158,6 @@ public class UIManager : MonoBehaviour
         t.Q<Foldout>("factReasonLabel").text = data.contents.fact_reason.header_text;
         generateSectionsDOM(t.Q<VisualElement>("factReasonContents"), data.contents.fact_reason.sections);
         container.Add(t);
+        generateTableOfContentDOM(uIDocument.rootVisualElement.Q<ScrollView>("tableContainer"), data.contents.fact_reason.sections);
     }
 }
